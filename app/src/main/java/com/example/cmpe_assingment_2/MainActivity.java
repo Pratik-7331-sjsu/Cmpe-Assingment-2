8f19bb4120606f4671387b53a8df266087422b61
Pratik-7331-sjsu <pratikrajnikant.shah@sjsu.edu>
2023-10-09 22:14:40 -0700

    Assignment-2 completed

---
+++ app/src/main/java/com/example/cmpe_assingment_2/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.cmpe_assingment_2;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Launch(View v) {
+        TextView urlTextView = (TextView) findViewById(R.id.urlTextView);
+        String url = urlTextView.getText().toString();
+        //Incase of empty text box there is no action on click of the button
+        if(!url.equals("")) {
+            Intent urlIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + url));
+            startActivity(urlIntent);
+        }
+    }
+
+    public void Ring(View v) {
+        TextView phoneTextView = (TextView) findViewById(R.id.phoneTextView);
+        String phone = phoneTextView.getText().toString();
+        //Incase of empty text box
+        if(!phone.equals("")) {
+            Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phone));
+            startActivity(phoneIntent);
+        }
+    }
+
+    public void CloseApp(View v) {
+        finish();
+    }
+}
